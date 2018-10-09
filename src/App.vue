3c52154e71c9c4c17115de86dcfce783b0f1a802
Hendrik Lösch <loeschhendrik@gmail.com>
2018-10-09 20:24:02 +0200

    Added vuetify and removed example code.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <div id="app">
+  <v-app id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
     </div>
     <router-view/>
-  </div>
+  </v-app>
 </template>
 
 <style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container id="home">
+   <v-card>
+     <v-card-text>
+       <span class="text-center">
+          Welcome
+       </span>
+     </v-card-text>
+   </v-card>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
   }
 }
 </script>
