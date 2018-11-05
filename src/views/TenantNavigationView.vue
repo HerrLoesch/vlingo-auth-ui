f913bb49ec01c08ebd6b689d2fa18d414b287b3a
Hendrik Lösch <hendrik.loesch@saxsys.de>
2018-11-05 15:37:42 +0100

    Restyling navigation

--- src/views/TenantNavigationView.vue
+++ src/views/TenantNavigationView.vue
@@ -1,86 +1,122 @@
 <template>
     <v-container fluid>
-        <v-toolbar app fixed clipped-left>
+        <v-toolbar app fixed prominent clipped-left>
+            <v-toolbar-side-icon @click="drawer = !drawer"><v-icon>dehaze</v-icon></v-toolbar-side-icon>
             <v-toolbar-title>
                 Vlingo
             </v-toolbar-title>
             <v-spacer></v-spacer>
             <v-toolbar-items>
+                <v-btn icon>
+                    <v-icon>notifications</v-icon>
+                </v-btn>
                 <v-menu offset-y offset-x>
                     <v-toolbar-title slot="activator">
                         <v-btn icon>
-                            <v-icon>account_circle</v-icon>
+                            <v-icon>person</v-icon>
                         </v-btn>
                     </v-toolbar-title>
 
                     <v-list>
                         <v-list-tile>
                             <v-list-tile-title>
+                                <v-icon left>person</v-icon>
                                 Profile
                             </v-list-tile-title>
                         </v-list-tile>
                         <v-divider></v-divider>
                         <v-list-tile @click="logOut()">
-                            <v-list-tile-title>Logout</v-list-tile-title>
+                            <v-list-tile-title>
+                                <v-icon left>power_settings_new</v-icon>
+                                Logout
+                            </v-list-tile-title>
+                        </v-list-tile>
+                    </v-list>
+                </v-menu>
+                <v-menu offset-y offset-x>
+                    <v-toolbar-title slot="activator">
+                        <v-btn icon>
+                            <v-icon>more_vert</v-icon>
+                        </v-btn>
+                    </v-toolbar-title>
+
+                    <v-list>
+                        <v-list-tile>
+                            <v-list-tile-title>
+                                <v-icon left>code</v-icon>
+                                Code
+                            </v-list-tile-title>
+                        </v-list-tile>
+                        <v-divider></v-divider>
+                        <v-list-tile >
+                            <v-list-tile-title>
+                                <v-icon left>help</v-icon>
+                                Documentation
+                            </v-list-tile-title>
                         </v-list-tile>
                     </v-list>
                 </v-menu>
             </v-toolbar-items>
         </v-toolbar>
         <v-navigation-drawer
-                app
+                width="200"
                 clipped
-                permanent
+                app
+                v-model="drawer"
         >
             <v-list>
 
+                <v-subheader>Users</v-subheader>
+                <v-list-tile v-for="(userView, i) in userViews" :key="i" @click="goTo(userView.name)">
+                    <v-list-tile-action>
+                        <v-icon>{{userView.icon}}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-title v-text="userView.title"></v-list-tile-title>
+                </v-list-tile>
 
-                    <v-list-tile v-for="(userView, i) in userViews" :key="i" @click="goTo(userView.name)">
-                        <v-list-tile-action>
-                            <v-icon>{{userView.icon}}</v-icon>
-                        </v-list-tile-action>
-                        <v-list-tile-title v-text="userView.title"></v-list-tile-title>
-                    </v-list-tile>
+                <v-divider></v-divider>
+                <v-subheader>Groups</v-subheader>
+                <v-list-tile v-for="(groupView, i) in groupViews" :key="i+10" @click="goTo(groupView.name)">
+                    <v-list-tile-action>
+                        <v-icon>{{groupView.icon}}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-title v-text="groupView.title"></v-list-tile-title>
+                </v-list-tile>
 
-                <v-list-group name="permissions">
-                    <v-list-tile slot="activator">
-                        <v-list-tile-title>Permissions</v-list-tile-title>
-                    </v-list-tile>
+                <v-divider></v-divider>
+                <v-subheader>Roles</v-subheader>
+                <v-list-tile v-for="(roleView, i) in roleViews" :key="i+100" @click="goTo(roleView.name)">
+                    <v-list-tile-action>
+                        <v-icon outline class="outlined">{{roleView.icon}}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-title v-text="roleView.title"></v-list-tile-title>
+                </v-list-tile>
 
-                    <v-list-tile v-for="(permissionView, i) in permissionViews" :key="i"
-                                 @click="goTo(permissionView.name)">
-                        <v-list-tile-action>
-                            <v-icon>{{permissionView.icon}}</v-icon>
-                        </v-list-tile-action>
-                        <v-list-tile-title v-text="permissionView.title"></v-list-tile-title>
-                    </v-list-tile>
-                </v-list-group>
+                <v-divider></v-divider>
+                <v-subheader>Permissions</v-subheader>
+                <v-list-tile v-for="(permissionView, i) in permissionViews" :key="i+1000"
+                             @click="goTo(permissionView.name)">
+                    <v-list-tile-action>
+                        <v-icon>{{permissionView.icon}}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-title v-text="permissionView.title"></v-list-tile-title>
+                </v-list-tile>
 
-                <v-list-group name="groups">
-                    <v-list-tile slot="activator">
-                        <v-list-tile-title>Groups</v-list-tile-title>
-                    </v-list-tile>
+                <v-divider></v-divider>
+                <v-list-tile v-for="(miscView, i) in miscViews" :key="i+10000"
+                             @click="goTo(miscView.name)">
+                    <v-list-tile-action>
+                        <v-icon>{{miscView.icon}}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-title v-text="miscView.title"></v-list-tile-title>
+                </v-list-tile>
 
-                    <v-list-tile v-for="(groupView, i) in groupViews" :key="i" @click="goTo(groupView.name)">
-                        <v-list-tile-action>
-                            <v-icon>{{groupView.icon}}</v-icon>
-                        </v-list-tile-action>
-                        <v-list-tile-title v-text="groupView.title"></v-list-tile-title>
-                    </v-list-tile>
-                </v-list-group>
-
-                <v-list-group name="roles">
-                    <v-list-tile slot="activator">
-                        <v-list-tile-title>Roles</v-list-tile-title>
-                    </v-list-tile>
-
-                    <v-list-tile v-for="(roleView, i) in roleViews" :key="i" @click="goTo(roleView.name)">
-                        <v-list-tile-action>
-                            <v-icon>{{roleView.icon}}</v-icon>
-                        </v-list-tile-action>
-                        <v-list-tile-title v-text="roleView.title"></v-list-tile-title>
-                    </v-list-tile>
-                </v-list-group>
+                <v-list-tile @click="logOut">
+                    <v-list-tile-action>
+                        <v-icon>power_settings_new</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-title>Log Out</v-list-tile-title>
+                </v-list-tile>
 
 
             </v-list>
@@ -105,44 +141,51 @@
                 {
                     title: "Users",
                     name: "/listUsers",
-                    icon: "people"
+                    icon: "supervisor_account"
+                },
+                {
+                    title: "Add User",
+                    name: "/registerUser",
+                    icon: "person_add"
                 }
             ],
             groupViews: [
                 {
-                    title: "Add Group",
+                    title: "Groups",
                     name: "/dummy",
-                    icon: "add_circle_outline"
+                    icon: "group"
                 },
                 {
-                    title: "List Groups",
+                    title: "Add Group",
                     name: "/dummy",
-                    icon: "list"
+                    icon: "group_add"
                 }
             ],
             permissionViews: [
                 {
-                    title: "Add Permission",
-                    name: "/dummy",
-                    icon: "add_circle_outline"
-                },
-                {
-                    title: "List Permissions",
+                    title: "Permissions",
                     name: "/dummy",
                     icon: "list"
+                },
+                {
+                    title: "Add Permission",
+                    name: "/dummy",
+                    icon: "lock"
                 }
             ],
             roleViews: [
                 {
-                    title: "Add Role",
+                    title: "Roles",
                     name: "/dummy",
-                    icon: "add_circle_outline"
+                    icon: "people_outline"
                 },
                 {
-                    title: "List Roles",
+                    title: "Add Role",
                     name: "/dummy",
-                    icon: "list"
+                    icon: "person_outline"
                 }
+            ],
+            miscViews: [
             ]
         }),
         methods: {
