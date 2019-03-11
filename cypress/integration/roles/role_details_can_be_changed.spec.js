9efe7763a158526926a40abaa06a0dcd9d15ec73
Hendrik Lösch <loeschhendrik@googlemail.com>
2019-03-11 12:49:36 +0100

    Fixed broken test that checks if a role can be edited.

--- cypress/integration/roles/role_details_can_be_changed.spec.js
+++ cypress/integration/roles/role_details_can_be_changed.spec.js
@@ -1,6 +1,6 @@
 describe("When data of a role is changed", () => {
 
-    let usedRoleData;
+    let existingRoleData;
     let newRoleData;
 
     before(() => {
@@ -8,13 +8,15 @@ describe("When data of a role is changed", () => {
         cy.visit("/#/listRoles")
 
         cy.fixture("existingRole").then((role) => {
-            usedRoleData = role
+            existingRoleData = role
 
-            cy.searchListFor("#roles-list", usedRoleData.name)
+            cy.searchListFor("#roles-list", existingRoleData.name)
             cy.get("#roles-list").contains("edit").click()
 
             cy.fixture("newRole").then((newRole) => {
                 newRoleData = newRole
+                
+                newRoleData.name = undefined
                 cy.enterRoleDetails(newRole, ".v-dialog--active")
 
                 cy.get(".v-dialog--active").contains("Save").click()
@@ -23,14 +25,14 @@ describe("When data of a role is changed", () => {
     })
 
     it("then the old data can no longer be found.", () => {
-        cy.searchListFor("#roles-list", usedRoleData.name)
+        cy.searchListFor("#roles-list", existingRoleData.name)
 
-        cy.get("#roles-list").should("not.contain", usedRoleData.description)
+        cy.get("#roles-list").should("not.contain", existingRoleData.description)
     })
 
     it("the the new data is to be found.", () => {
 
-        cy.searchListFor("#roles-list", newRoleData.userName)
+        cy.searchListFor("#roles-list", existingRoleData.name)
 
         cy.get("#roles-list").within(() => {
             cy.get(".v-table").should("contain", newRoleData.description)
